e0264d2ec3a6b3f1938279c572a9332ee950a3d8
Nicolas Hidalgo Rotunno <nhidalgorotunno@kidscarehh.com>
2025-09-19 13:26:40 -0500

    Adding Coffee App

--- README.md
+++
@@ -1,2 +0,0 @@
-# Introduction-to-Kotlin
-This is a repository where I will have all my introductory projects for Kotlin. Most specifically from the ..... course I will be working on. 
---
+++ app/src/main/java/com/example/practicecoffeefirebaseapp/SplashActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.practicecoffeefirebaseapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practicecoffeefirebaseapp.databinding.ActivitySplash2Binding
+
+class SplashActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivitySplash2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplash2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+}
